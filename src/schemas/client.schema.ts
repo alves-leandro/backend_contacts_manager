85ab2c1cd45ee4b632a812735163b79b74617d0a
alves-leandro <l.alves85@live.com>
2023-05-22 22:23:21 -0300

    feat: phoneRegex

--- src/schemas/client.schema.ts
+++ src/schemas/client.schema.ts
@@ -1,11 +1,13 @@
 import { z } from "zod"
 
+const phoneRegex = /^\(\d{2}\)\s\d{4,5}-\d{4}$/;
+
 const clientSchema = z.object({
     id: z.string(),
     name: z.string(),
     email: z.string().email(),
     password: z.string(),
-    phone: z.string()
+    phone: z.string().regex(phoneRegex)
 })
 
 const clientSchemaRequest = clientSchema.omit({
