8e1f2c4bc28aadb110dd9bedd4eac534f793aae8
alves-leandro <l.alves85@live.com>
2023-05-28 13:03:25 -0300

    fix: swagger

--- src/services/contact/updateContact.service.ts
+++ src/services/contact/updateContact.service.ts
@@ -6,7 +6,7 @@ import { Contact } from "../../entities/contact.entitie";
 import { contactSchema } from "../../schemas/contact.schema";
 
 
-const updateContactService = async (data: TContactUpdateRequest, contactId: string): Promise<TContactResponse> => {
+const updateContactService = async (data: any, contactId: string): Promise<TContactResponse> => {
     const contactsRepository: Repository<Contact> = AppDataSource.getRepository(Contact)
     const oldContact: Contact | null = await contactsRepository.findOneBy({ id: contactId })
 
@@ -17,11 +17,11 @@ const updateContactService = async (data: TContactUpdateRequest, contactId: stri
     const newContactData = contactsRepository.create({
         ...(oldContact || {}),
         ...data,
-        secondEmail: data.secondEmail ? data.secondEmail : (oldContact?.secondEmail || ''),
-        secondPhone: data.secondPhone ? data.secondPhone : (oldContact?.secondPhone || ''),
-        githubUser: data.githubUser ? data.githubUser : (oldContact?.githubUser || ''),
-        linkedinUser: data.linkedinUser ? data.linkedinUser : (oldContact?.linkedinUser || ''),
-        portifolioUrl: data.portifolioUrl ? data.portifolioUrl : (oldContact?.portifolioUrl || ''),
+        secondEmail: data.secondEmail ? data.secondEmail : (oldContact?.secondEmail || null),
+        secondPhone: data.secondPhone ? data.secondPhone : (oldContact?.secondPhone || null),
+        githubUser: data.githubUser ? data.githubUser : (oldContact?.githubUser || null),
+        linkedinUser: data.linkedinUser ? data.linkedinUser : (oldContact?.linkedinUser || null),
+        portifolioUrl: data.portifolioUrl ? data.portifolioUrl : (oldContact?.portifolioUrl || null),
     });
 
     await contactsRepository.save(newContactData)
