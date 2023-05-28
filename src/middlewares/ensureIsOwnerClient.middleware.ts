e64b848ccdead9bcf34ba1f8fdf79a4de5d311a5
alves-leandro <l.alves85@live.com>
2023-05-28 13:50:16 -0300

    fix: UUID validation

--- src/middlewares/ensureIsOwnerClient.middleware.ts
+++ src/middlewares/ensureIsOwnerClient.middleware.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "../data-source";
 import { Client } from "../entities/client.entitie";
 
+const isValidUUID = (uuid: string): boolean => {
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidRegex.test(uuid);
+};
+
 const ensureIsOwnerMiddlewareClient = async (
   req: Request,
   res: Response,
@@ -11,6 +17,12 @@ const ensureIsOwnerMiddlewareClient = async (
 
   const clientId: string = req.params.id;
   const loggedInClientId: string = res.locals.clientId;
+
+  if (!isValidUUID(clientId)) {
+    return res.status(400).json({
+      message: "Invalid client ID",
+    });
+  }
 
   const client = await clientsRepository.findOne({
     where: {
--- src/middlewares/ensureIsOwnerContact.middleware.ts
+++ src/middlewares/ensureIsOwnerContact.middleware.ts
@@ -2,6 +2,12 @@ import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "../data-source";
 import { Contact } from "../entities/contact.entitie";
 
+const isValidUUID = (uuid: string): boolean => {
+  const uuidRegex =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+  return uuidRegex.test(uuid);
+};
+
 const ensureIsOwnerMiddlewareContact = async (
   req: Request,
   res: Response,
@@ -11,6 +17,12 @@ const ensureIsOwnerMiddlewareContact = async (
 
   const contactId: string = req.params.id;
   const clientId: string = res.locals.clientId;
+
+  if (!isValidUUID(contactId)) {
+    return res.status(400).json({
+      message: "Invalid contact ID",
+    });
+  }
 
   const contact = await contactsRepositoy.findOne({
     where: {
