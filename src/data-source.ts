ab92f09cd40cc6f1b5dab5826f6baf4abed102a8
alves-leandro <l.alves85@live.com>
2023-05-28 14:49:58 -0300

    fix: env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,36 +1,35 @@
-import 'dotenv/config';
-import 'reflect-metadata';
-import path from 'path';
-import { DataSource, DataSourceOptions } from 'typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import "dotenv/config";
+import "reflect-metadata";
+import path from "path";
+import { DataSource, DataSourceOptions } from "typeorm";
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiePath: string = path.join(__dirname, './entities/**.{ts,js}');
-  const migrationsPath: string = path.join(__dirname, './migrations/**.{ts,js}');
+  const entitiePath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const migrationsPath: string = path.join(
+    __dirname,
+    "./migrations/**.{ts,js}"
+  );
 
   const nodeEnv: string | undefined = process.env.NODE_ENV;
 
-  if (nodeEnv === 'test') {
+  if (nodeEnv === "test") {
     return {
-      type: 'sqlite',
-      database: ':memory:',
+      type: "sqlite",
+      database: ":memory:",
       synchronize: true,
       entities: [entitiePath],
     };
   }
 
-  // const dbUrl: string | undefined = process.env.DATABASE_URL;
-  // console.log(dbUrl);
+  const dbUrl: string | undefined = process.env.DATABASE_URL;
+  console.log(dbUrl);
 
-  // if (!dbUrl) throw new Error('Env var DATABASE_URL does not exists');
+  if (!dbUrl) throw new Error("Env var DATABASE_URL does not exists");
 
   return {
-    type: 'postgres',
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
+    type: "postgres",
+    url: dbUrl,
     synchronize: false,
     logging: true,
     migrations: [migrationsPath],
